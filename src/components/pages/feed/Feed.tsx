37e07f33d3640db018ba82ddf1a484408232025f
luis <herrera.espejo.luis@gmail.com>
2025-05-29 20:12:32 -0500

    update feed content

--- src/components/pages/feed/Feed.tsx
+++ src/components/pages/feed/Feed.tsx
@@ -1,26 +1,12 @@
-import { NavLink } from "react-router-dom";
-import { ArrowIcon } from "../../../helpers/ArrowIcon";
-import { GetRecipes } from "../../../helpers/GetRecipes";
+//import { NavLink } from "react-router-dom";
+//import { ArrowIcon } from "../../../helpers/ArrowIcon";
+//import { GetRecipes } from "../../../helpers/GetRecipes";
 import "./Feed.css";
 
 export const Feed = () => {
-  const {recipes} = GetRecipes();
-
   return (
     <section className="feed">
-      {recipes.map(({ id, name_recipe, photo, type_food, }) => (
-        <article key={id}>
-          <img src={photo} alt={name_recipe} />
-
-          <div>
-            <h2>{name_recipe}</h2>
-            <span>{type_food}</span>
-            <NavLink to="#">
-              Receta <ArrowIcon />
-            </NavLink>
-          </div>
-        </article>
-      ))}
+      <h1>alsjcn</h1>
     </section>
   );
 };
